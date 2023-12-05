23081d1aeb44fc9e73058f95f5486469ef79209f
Oscar Ramirez <ramirez7oscar8203@gmail.com>
2023-12-04 19:50:35 -0500

    Estructura base de Flutter

---
+++ sistema_restaurante/README.md
@@ -0,0 +1,3 @@
+# sistema_restaurante
+
+A new Flutter project.
---
+++ sistema_restaurante/android/app/src/main/kotlin/com/example/sistema_restaurante/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sistema_restaurante
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
